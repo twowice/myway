56a2b1717371b7c9507a7ad542cef63210c235d8
kimsuhyeon <kshyunn12@gmail.com>
2025-12-16 14:33:53 +0900

    [feat] 팝업창에 사용자 지정 className 추가

--- components/popup/onefunction.tsx
+++ components/popup/onefunction.tsx
@@ -11,9 +11,11 @@ import {
   DialogTrigger,
 } from "../ui/dialog";
 import { Button } from "../ui/button/button";
+import { cn } from "@/lib/utils";
 
 /**
  * @typedef {object} OneFunctionPopupProps
+ * @property {string} className - 팝업의 전체에 적용하고 싶은 style을 받는 변수
  * @property {ReactNode} dialogTrigger - 팝업창을 열기 위한 트리거 컴포넌트입니다. (예: <Button>보기</Button>)
  * @property {string} title - 팝업창의 제목입니다.
  * @property {ReactNode} body - 팝업창의 본문 내용입니다. ReactNode 타입으로 다양한 컴포넌트를 전달할 수 있습니다.
@@ -24,17 +26,22 @@ import { Button } from "../ui/button/button";
 /**
  * 하나의 버튼으로 특정 동작을 수행하거나 정보 확인 후 팝업을 닫는 공용 팝업 컴포넌트입니다.
  * 주로 알림, 정보 제공, 단일 선택 등의 상황에 사용됩니다.
+ * className으로 적용하고 싶은 sytle을 전체 팝업 박스에 지정하는 것이 가능합니다.
+ * 이 경우 기본 Dialog에 적용된 max-w-[calc(100%-2rem)] sm:max-w-lg 값이 삭제되어 반응형 구현이 사라지게 됩니다.
+ * className으로 팝업창의 전체 크기를 조절하시고 싶으시면 lg:max-w-xxx, sm:max-w-xxx와 같은 형태로 반응형 크기 지정을 하셔야합니다.
  *
  * @param {OneFunctionPopupProps} props - 팝업에 전달될 속성들.
  * @returns {React.ReactElement} OneFunction 팝업 컴포넌트.
  */
 export const OneFunctionPopup = ({
+  className,
   dialogTrigger,
   title,
   body,
   buttonTitle,
   callback = () => {},
 }: {
+  className?: string;
   dialogTrigger: ReactNode; //팝업창 오픈 버튼이자 팝업창 오픈 전의 화면에 보여질 컴포넌트
   title: string; //팝업창 제목 (좌상단 가장 큰 글자)
   body: ReactNode; //팝업창 바디에 들어갈 컴포넌트
@@ -44,7 +51,12 @@ export const OneFunctionPopup = ({
   return (
     <Dialog>
       <DialogTrigger asChild>{dialogTrigger}</DialogTrigger>
-      <DialogContent className="flex flex-col bg-white w-min-8xl p-[16px] gap-[16px]">
+      <DialogContent
+        className={cn(
+          "flex flex-col bg-white max-w-none p-4 gap-4",
+          className ? "lg:max-w-none sm:max-w-none max-w-none " + className : ""
+        )}
+      >
         <DialogHeader>
           <DialogTitle>
             <div>
--- components/popup/twofunction.tsx
+++ components/popup/twofunction.tsx
@@ -11,9 +11,11 @@ import {
   DialogTrigger,
 } from "../ui/dialog";
 import { Button } from "../ui/button/button";
+import { cn } from "@/lib/utils";
 
 /**
  * @typedef {object} ConfirmationPopupProps
+ * @property {string} className - 팝업의 전체에 적용하고 싶은 style을 받는 변수
  * @property {ReactNode} dialogTrigger - 팝업창을 열기 위한 트리거 컴포넌트입니다. (예: <Button>확인</Button>)
  * @property {string} title - 팝업창의 제목입니다.
  * @property {ReactNode} body - 팝업창의 본문 내용입니다. ReactNode 타입으로 다양한 컴포넌트를 전달할 수 있습니다.
@@ -27,12 +29,15 @@ import { Button } from "../ui/button/button";
  * 사용자에게 어떤 행동에 대해서 두 가지 기능 중 하나를 결정하게 하는 공용 팝업 컴포넌트입니다.
  * 이 팝업은 DialogTrigger로 팝업을 열고, DialogFooter에 두가지의 버튼을 포함하지만, 두 버튼의 색상이 동일합니다.
  * 기본 동작은 ConfirmationPopup과 동일합니다. 다른 점은 DialogFooter의 색상 뿐입니다.
- *
+ * className으로 적용하고 싶은 sytle을 전체 팝업 박스에 지정하는 것이 가능합니다.
+ * 이 경우 기본 Dialog에 적용된 max-w-[calc(100%-2rem)] sm:max-w-lg 값이 삭제되어 반응형 구현이 사라지게 됩니다.
+ * className으로 팝업창의 전체 크기를 조절하시고 싶으시면 lg:max-w-xxx, sm:max-w-xxx와 같은 형태로 반응형 크기 지정을 하셔야합니다.
  * @param {ConfirmationPopupProps} props - 팝업에 전달될 속성들.
  * @returns {React.ReactElement} Confirmation 팝업 컴포넌트.
  */
 
 export const TwoFunctionPopup = ({
+  className,
   dialogTrigger,
   title,
   body,
@@ -41,6 +46,7 @@ export const TwoFunctionPopup = ({
   leftCallback = () => {},
   rightCallback = () => {},
 }: {
+  className?: string;
   dialogTrigger: ReactNode; //팝업창 오픈 버튼이자 팝업창 오픈 전의 화면에 보여질 컴포넌트
   title: string; //팝업창 제목 (좌상단 가장 큰 글자)
   body: ReactNode; //팝업창 바디에 들어갈 컴포넌트
@@ -52,7 +58,12 @@ export const TwoFunctionPopup = ({
   return (
     <Dialog>
       <DialogTrigger asChild>{dialogTrigger}</DialogTrigger>
-      <DialogContent className="flex flex-col bg-white w-min-8xl p-[16px] gap-[16px]">
+      <DialogContent
+        className={cn(
+          "flex flex-col bg-white max-w-none p-4 gap-4",
+          className ? "lg:max-w-none sm:max-w-none max-w-none " + className : ""
+        )}
+      >
         <DialogHeader>
           <DialogTitle>
             <div>
