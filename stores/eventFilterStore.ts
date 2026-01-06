3c00cf2d456492b654cc233a0f58632e1d4c759b
hyogi <rhkdgur120@naver.com>
2026-01-06 12:44:03 +0900

    [fix] input 글씨 많아질때 ui 깨짐 수정, searchbar가 빈 문자열을 보내서 store keyword가 이미 존재하면 무시해서 덮어쓰기 불가 문제 해결, 마커 클릭시 해당 이벤트 보여짐 #87

--- stores/eventFilterStore.ts
+++ stores/eventFilterStore.ts
@@ -2,12 +2,24 @@ import { create } from 'zustand';
 
 interface EventFilterState {
   region: string;
+  keyword: string;
+
   setRegion: (region: string) => void;
+  setKeyword: (keyword: string) => void;
+
   resetRegion: () => void;
+  resetKeyword: () => void;
+  resetAll: () => void;
 }
 
 export const useEventFilterStore = create<EventFilterState>((set) => ({
   region: 'all',
-  setRegion: (region) => set({ region }),
+  keyword: '',
+
+  setKeyword: (keyword) => { set({ keyword }); },
+  setRegion: (region) => { set({ region }); },
+
   resetRegion: () => set({ region: 'all' }),
+  resetKeyword: () => set({ keyword: '' }),
+  resetAll: () => set({ region: 'all', keyword: '' }),
 }));
